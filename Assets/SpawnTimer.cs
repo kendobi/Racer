00796ccd927fa2917af4973d758176315b95629e
Kenny Kong <kenny@n3twork.com>
2018-11-05 15:13:10 -0800

    update

---
+++ Assets/SpawnTimer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnTimer : MonoBehaviour {
+
+	public GameObject prefab;
+	public float InstantiationTimer = 2f;
+
+	void Update () {
+		CreatePrefab();
+	}
+
+	void CreatePrefab()
+	{
+		InstantiationTimer -= Time.deltaTime;
+		if (InstantiationTimer <= 0)
+		{
+			Instantiate(prefab, gameObject.transform.position, Quaternion.identity);
+			InstantiationTimer = 2f;
+		}
+	}
+}
